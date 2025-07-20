35f902fe579f7e91b8c2289d54c11c686478e70b
GoldenaArcher <25376645+GoldenaArcher@users.noreply.github.com>
2025-07-20 18:33:35 -0400

    Add buildings data and implement rectangle visualization in D3.js

--- 13/js/main.js
+++ 13/js/main.js
@@ -21,3 +21,27 @@ circles
   .attr("cy", 250)
   .attr("r", (d) => d)
   .attr("fill", "red");
+
+d3.json('data/buildings.json').then(data => {
+    // Add an SVG to the #chart-area  div in the index.html file. Give it a width of 500px and a height of 500px.
+    const svg = d3.select('#rect-area')
+    .append('svg')
+    .attr('height', 500)
+    .attr('width', 500)
+
+    // Write a forEach() loop for your data array to convert the height values from strings to numbers.
+    data.forEach(el => {
+        el.height = parseInt(el.height)
+    });
+    
+    // Use a data join to add a rectangle for each building in the dataset to your SVG.
+    const rects = svg.selectAll('rect').data(data);
+
+    rects.enter()
+    .append('rect')
+    .attr('x', (_, i) => i * 70 + 50)
+    .attr('y', 50)
+    .attr('width', 50)
+    .attr('height', d => d.height)
+    .attr('fill', 'gray')
+})
