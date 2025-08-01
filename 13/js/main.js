0c109af5831a520336b1c90a9c450134d75cce1d
GoldenaArcher <25376645+GoldenaArcher@users.noreply.github.com>
2025-07-31 22:35:21 -0400

    Update building heights in JSON and enhance D3.js visualization with proper scaling and rectangle attributes

--- 13/js/main.js
+++ 13/js/main.js
@@ -1,8 +1,8 @@
 /*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    2.8 - Activity: Your first visualization!
-*/
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    2.8 - Activity: Your first visualization!
+ */
 
 const data = [25, 20, 10, 12, 15];
 
@@ -22,26 +22,43 @@ circles
   .attr("r", (d) => d)
   .attr("fill", "red");
 
-d3.json('data/buildings.json').then(data => {
-    // Add an SVG to the #chart-area  div in the index.html file. Give it a width of 500px and a height of 500px.
-    const svg = d3.select('#rect-area')
-    .append('svg')
-    .attr('height', 500)
-    .attr('width', 500)
+d3.json("data/buildings.json").then((data) => {
+  // Add an SVG to the #chart-area  div in the index.html file. Give it a width of 500px and a height of 500px.
+  const svg = d3
+    .select("#rect-area")
+    .append("svg")
+    .attr("height", 500)
+    .attr("width", 500);
 
-    // Write a forEach() loop for your data array to convert the height values from strings to numbers.
-    data.forEach(el => {
-        el.height = parseInt(el.height)
-    });
-    
-    // Use a data join to add a rectangle for each building in the dataset to your SVG.
-    const rects = svg.selectAll('rect').data(data);
+  // Write a forEach() loop for your data array to convert the height values from strings to numbers.
+  data.forEach((el) => {
+    el.height = parseInt(el.height);
+  });
 
-    rects.enter()
-    .append('rect')
-    .attr('x', (_, i) => i * 70 + 50)
-    .attr('y', 50)
-    .attr('width', 50)
-    .attr('height', d => d.height)
-    .attr('fill', 'gray')
-})
+  const x = d3
+    .scaleBand()
+    .domain(data.map((el) => el.name))
+    .range([0, 400])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
+  const y = d3
+    .scaleLinear()
+    // .domain([0, Math.max(...data.map((d) => d.height))])
+    .domain([0, d3.max(data, d => d.height)])
+    .range([0, 400]);
+
+  // Use a data join to add a rectangle for each building in the dataset to your SVG.
+  const rects = svg.selectAll("rect").data(data);
+
+  rects
+    .enter()
+    .append("rect")
+    // .attr("x", (_, i) => i * 70 + 50)
+    .attr('x', d => x(d.name))
+    .attr("y", 50)
+    .attr('width', x.bandwidth)
+    // .attr("width", 50)
+    .attr("height", (d) => y(d.height))
+    .attr("fill", "gray");
+});
